ee838466c10f29b7505b0f7886642058ff622c02
Ali Khalili <ali.khll.1994@gmail.com>
2017-09-05 14:46:57 +0430

    index store added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+import './components/App.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './components/App'
 
-import './App.css'
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import reducers from './reducers'
+
+const store = createStore(reducers); 
+
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
 );
