449707750187f6b98881142c60a37b8ada1c0f76
Ali Khalili <ali.khll.1994@gmail.com>
2017-09-09 19:56:36 +0430

    index js router config added

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import './components/App.css'
 
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Init from './components/Init'
 import App from './components/App'
+import Signup from './components/Signup'
 
 import { Provider } from 'react-redux'
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import reducers from './reducers'
 
-const store = createStore(reducers); 
+const store = createStore(reducers);
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <div>
+                <Route exact path="/" component={Init} />
+                <Route path="/App" component={App} />
+                <Route path="/Signup" component={Signup} />
+            </div>
+        </Router>
     </Provider>
     , document.getElementById('root')
 );
