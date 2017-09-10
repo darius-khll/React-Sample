925dbd7b4fe65064f05f4fff1de53d3f0d953897
Ali Khalili <ali.khll.1994@gmail.com>
2017-09-10 18:01:16 +0430

    mobx observable state added

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {observable} from 'mobx';
+import {observer} from "mobx-react";
 
-class Signup extends Component {
+let time = observable({
+    val: new Date()
+});
+
+// @observer
+const Signup = observer(class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { src: 'https://facebook.github.io/react/img/logo.svg' }
+
+         setInterval(() => {
+            time.val = new Date();
+        }, 1000);
+    }
+
+    changeSrc() {
+        this.setState({ src: 'nothing' });
+    }
+
     render() {
+        let img = this.state.src;
         return (
             <div>
-                <h1>Signup</h1>
+                <h1>Signup - {time.val.toLocaleTimeString()}</h1>
+                <img height="100" width="100" src={img} /> <br />
+                <button onClick={() => this.changeSrc()}>Change</button> <br />
                 <Link to="/App">App</Link>
             </div>
 
         )
     }
-}
+})
 
 export default Signup;
