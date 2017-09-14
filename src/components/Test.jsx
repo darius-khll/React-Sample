9f930e982a7452783ee099d4692160f35ca61715
AliKhll <ali.khll.1994@gmail.com>
2017-09-14 14:59:46 +0430

    Child compoennt manage parent state

---
+++ src/components/Test.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { observer } from "mobx-react";
+
+// export const Test = () => {
+//     return(
+//         <h1>Test.jsx</h1>
+//     )
+// }
+
+const Test = observer(class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.store = this.props.store;
+    }
+    changePatentState() {
+        this.store.parent.child = 0;
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Test Component {this.store.parent.child}</h4>
+                <button onClick={e => this.changePatentState()}>Update Parent</button>
+            </div>
+        )
+    }
+});
+
+export default Test;
