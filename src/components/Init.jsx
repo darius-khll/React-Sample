ea5633e72796a8d3482291950e3a482ca6ec484d
AliKhll <ali.khll.1994@gmail.com>
2017-09-16 20:58:59 +0430

    dynamiccaly passing parameter to link routing added

--- src/components/Init.jsx
+++ src/components/Init.jsx
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Init extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            appId: 0
+        }
+    }
+
+    onInputchange(e) {
+        this.setState({ appId: e.target.value })
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
-                <Link to="/app/1">App</Link> <br />
+                <input type="number" onChange={e => this.onInputchange(e)} />
+                <br />
+                <Link to={`/app/${this.state.appId}`}>App</Link> <br />
                 <Link to="/signup">Signup</Link>
             </div>
         )
