a3a68e9708e8507296e8e1d935c57bf018eae514
ali <ali>
2017-09-19 19:25:07 +0430

    grand parent added to router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Init from './components/Init'
 import App from './components/App'
 import Signup from './components/Signup'
+import GrandParent from './components/component1/GrandParent'
 
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
@@ -22,6 +23,7 @@ ReactDOM.render(
                 <Route exact path="/" component={Init} />
                 <Route path="/App/:id" component={App} />
                 <Route path="/Signup" component={Signup} />
+                <Route path="/GrandParent" component={GrandParent} />
             </div>
         </Router>
     </Provider>
