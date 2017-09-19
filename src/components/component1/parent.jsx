749dd339eda832ae3bb5ac9c018363e54852eb29
ali <ali>
2017-09-19 19:31:57 +0430

    parent component

--- src/components/component1/parent.jsx
+++ src/components/component1/parent.jsx
@@ -5,7 +5,6 @@ class Parent extends Component {
     render() {
         return (
             <div>
-                {/* binding change data functions */}
                 <Child changeData={this.props.changeData} />
             </div>
         )
